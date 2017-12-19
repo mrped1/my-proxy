655fbfaa08c94ba39e508b296772b71792d03209
mrped1 <34680382+mrped1@users.noreply.github.com>
2017-12-19 13:51:44 +0100

    Update defaults.js

--- config/defaults.js
+++ config/defaults.js
@@ -8,14 +8,4 @@ module.exports = {
   diff: null,
   dynamicPool: false,
   maxMinersPerConnection: 100,
-  donations: [
-    {
-      address: "4AVuRSRto6UK4f4arks3483xciDQkcpFKC45nq2Nd4eu61wdKjtEGqWMMUp2aemt8DCGRJxCsyK1nZ3GHPCTeoxQDA4boh3",
-      host: "pool.supportxmr.com",
-      port: 3333,
-      user: null,
-      pass: "donations",
-      percentage: 0.01 // 1%
-    }
-  ]
 };
